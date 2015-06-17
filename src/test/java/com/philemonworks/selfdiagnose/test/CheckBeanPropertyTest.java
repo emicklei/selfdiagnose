b7b88c9716f45c13c0ea7c8f5c2880eb23b7dd83
Ernest Micklei <ernest.micklei@gmail.com>
2015-06-17 15:52:18 +0200

    add test (example) for calling a static method for checking a bean

--- src/test/java/com/philemonworks/selfdiagnose/test/CheckBeanPropertyTest.java
+++ src/test/java/com/philemonworks/selfdiagnose/test/CheckBeanPropertyTest.java
@@ -7,28 +7,44 @@ import com.philemonworks.selfdiagnose.ExecutionContext;
 import com.philemonworks.selfdiagnose.check.CheckBeanProperty;
 
 public class CheckBeanPropertyTest extends TestCase {
-	public void testArrayListSize() throws DiagnoseException {
-		CheckBeanProperty task = new CheckBeanProperty();
-		task.setProperty("property");
-		task.setBean("${bean}");
-		task.setVariableName("var");
 
-		ExecutionContext ctx = new ExecutionContext();
-		ctx.setValue("bean", task);
-		assertFalse(task.run(ctx).isError());
-		assertEquals(ctx.getValue("var"),"property");
+    public static final String stopt() {
+        return "HELLO";
+    }
 
-	}
-	
-	public void testStringProperty() throws DiagnoseException {
-		CheckBeanProperty task = new CheckBeanProperty();
-		task.setMethod("toUpperCase");
-		task.setBean("Hello");
-		task.setVariableName("var");
+    public void testArrayListSize() throws DiagnoseException {
+        CheckBeanProperty task = new CheckBeanProperty();
+        task.setProperty("property");
+        task.setBean("${bean}");
+        task.setVariableName("var");
 
-		ExecutionContext ctx = new ExecutionContext();
-		assertFalse(task.run(ctx).isError());
-		assertEquals(ctx.getValue("var"),"HELLO");
+        ExecutionContext ctx = new ExecutionContext();
+        ctx.setValue("bean", task);
+        assertFalse(task.run(ctx).isError());
+        assertEquals(ctx.getValue("var"), "property");
 
-	}
+    }
+
+    public void testStringProperty() throws DiagnoseException {
+        CheckBeanProperty task = new CheckBeanProperty();
+        task.setMethod("toUpperCase");
+        task.setBean("Hello");
+        task.setVariableName("var");
+
+        ExecutionContext ctx = new ExecutionContext();
+        assertFalse(task.run(ctx).isError());
+        assertEquals(ctx.getValue("var"), "HELLO");
+
+    }
+
+    public void testCallStaticMethodProperty() throws DiagnoseException {
+        CheckBeanProperty task = new CheckBeanProperty();
+        task.setBean("${@com.philemonworks.selfdiagnose.test.CheckBeanPropertyTest@stopt()}");
+        task.setVariableName("var");
+
+        ExecutionContext ctx = new ExecutionContext();
+        assertFalse(task.run(ctx).isError());
+        assertEquals(ctx.getValue("var"), "HELLO");
+
+    }
 }
