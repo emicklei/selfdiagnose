0f8b98e2693e78068ab6c56ecdc62eff23afa1ae
Maurice Zeijen <mzeijen@bol.com>
2016-02-23 15:45:44 +0100

    Change Jamon Monitor report so that it doesn't use the HTML table anymore but uses the fact that we now have monospace font to create some structure.

--- src/main/java/com/philemonworks/selfdiagnose/report/vendor/ReportJamonMonitors.java
+++ src/main/java/com/philemonworks/selfdiagnose/report/vendor/ReportJamonMonitors.java
@@ -1,18 +1,14 @@
 package com.philemonworks.selfdiagnose.report.vendor;
 
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorComposite;
+import com.jamonapi.MonitorFactory;
+import com.philemonworks.selfdiagnose.*;
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
-
-import com.jamonapi.Monitor;
-import com.jamonapi.MonitorComposite;
-import com.jamonapi.MonitorFactory;
-import com.philemonworks.selfdiagnose.CompositeDiagnosticTaskResult;
-import com.philemonworks.selfdiagnose.DiagnoseException;
-import com.philemonworks.selfdiagnose.DiagnosticTask;
-import com.philemonworks.selfdiagnose.DiagnosticTaskResult;
-import com.philemonworks.selfdiagnose.ExecutionContext;
 
 public class ReportJamonMonitors extends DiagnosticTask {
     private static final long serialVersionUID = 4764469767469398343L;
@@ -29,13 +25,13 @@ public class ReportJamonMonitors extends DiagnosticTask {
             result.setPassedMessage("No monitor activity recorded");
             return;
         }
-        // sort monitors by label lexicographically 
+        // sort monitors by label lexicographically
         Arrays.sort(monitors,new Comparator<Monitor>() {
             public int compare(Monitor o1, Monitor o2) {
                 return o1.getLabel().compareTo(o2.getLabel());
-            }            
+            }
         });
-        
+
         CompositeDiagnosticTaskResult comResult = (CompositeDiagnosticTaskResult) result;
         for (Monitor each : monitors) {
             DiagnosticTaskResult localResult = new DiagnosticTaskResult(this);
@@ -44,57 +40,53 @@ public class ReportJamonMonitors extends DiagnosticTask {
             comResult.addResult(localResult);
         }
     }
-    
+
     private String composeRow(Monitor each) {
         StringBuilder sb = new StringBuilder();
-        sb.append("<table style='font-family:monospace;font-size:larger; width=100%'><tr>");
-        appendCell("hits", this.padded(Math.round(each.getHits())), sb);
-        appendCell("avg", this.padded(each.getAvg()), sb);
-        appendCell("stddev", this.padded(each.getStdDev()), sb);
-        appendCell("min", this.padded(each.getMin()), sb);        
-        appendCell("max", this.padded(each.getMax()), sb);
-        appendCell("first", this.formatDate(each.getFirstAccess()), sb);
-        appendCell("last", this.formatDate(each.getLastAccess()), sb);
-        sb.append("</tr></table>");
+        append("hits", this.padded(Math.round(each.getHits())), sb);
+        append("avg", this.padded(each.getAvg()), sb);
+        append("stddev", this.padded(each.getStdDev()), sb);
+        append("min", this.padded(each.getMin()), sb);
+        append("max", this.padded(each.getMax()), sb);
+        append("first", this.formatDate(each.getFirstAccess()), sb);
+        append("last", this.formatDate(each.getLastAccess()), sb);
         return sb.toString();
     }
-    
+
     private String padded(Double d) {
         String dString = Double.toString(d);
         if (d <= 99999999d && dString.length() > 8) // if d IS larger then do not chop
             return dString.substring(0,8);
         StringBuilder sb = new StringBuilder(dString);
-        for (int p = sb.length() ; p < 8 ; p++) sb.append("&nbsp;");
+        for (int p = sb.length() ; p < 8 ; p++) sb.append(' ');
         return sb.toString();
     }
-    
+
     private String padded(long l) {
         StringBuilder sb = new StringBuilder(Long.toString(l));
-        for (int p = sb.length() ; p < 8 ; p++) sb.append("&nbsp;");
+        for (int p = sb.length() ; p < 8 ; p++) sb.append(' ');
         return sb.toString();
-    }    
-        
+    }
+
     private String formatDate(Date aDate) {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         return df.format(aDate);
     }
-    
-    private void appendCell(String label, Object what, StringBuilder sb) {
+
+    private void append(String label, Object what, StringBuilder sb) {
         Object value = what;
         if (what instanceof Double) {
             value = Math.round(100f * (Double)what) / 100;
         }
-        sb.append("<td>");
-        sb.append(label).append('=').append(value);
-        sb.append("</td>");        
+        sb.append(label).append(": ").append(value);
     }
     /**
      * This method is redefined such that the run method will get a composed
      * result for its argument.
-     * 
+     *
      * @return CompositeDiagnosticTaskResult
      */
     public DiagnosticTaskResult createResult() {
         return new CompositeDiagnosticTaskResult(this);
-    }    
+    }
 }
